f2b66db5654a1861fda5f6125d7b5fd87605b151
ArentS <arent.sylanaj@gmail.com>
2016-12-25 18:32:08 +0100

    Many functionalities are added, v.1.0 of the app.

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from bson import json_util, ObjectId
 from app import mongo
 
@@ -18,7 +18,8 @@ def index():
     elif request.method == 'POST':
         data = request.form.to_dict()
         db.insert(data)
-        return json_util.dumps(data)
+        return render_template('success.html')
+        #return json_util.dumps(data)
     else:
         return 'Bad request'
 
@@ -39,3 +40,65 @@ def get_doc(id):
         #return "<p>You requested a document with ID" + id +'<p>/n ' + json_util.dumps(doc)
     else:
         return 'Bad request'
+    
+@mod_main.route('/lista', methods=['GET'])
+def lista():
+    dokumentat = mongo.db.arkep.find()
+    return render_template('lista.html', dokumentat = dokumentat)
+
+@mod_main.route('/remove/<string:id>', methods=['GET'])
+def remove_doc(id):
+    
+    #Return a mongo document with the specified id.
+    #return MongoDb Cursor
+    
+    db = mongo.db.arkep
+    
+    if request.method == 'GET':
+    
+        
+        doc =db.remove({"_id":ObjectId(id)})
+        #doc_json = json_util.dumps(doc)
+        #return render_template('doc.html' , doc = doc_json )
+        return redirect(url_for('main.lista'))
+        #return "<p>You requested a document with ID" + id +'<p>/n ' + json_util.dumps(doc)
+    else:
+        return 'Bad request'
+    
+@mod_main.route('/update/<string:id>', methods=['GET'])
+def get_doc_to_update(id):
+    
+    #Return a mongo document with the specified id.
+    #return MongoDb Cursor
+    
+    db = mongo.db.arkep
+    
+    if request.method == 'GET':
+    
+        doc = db.find_one({"_id":ObjectId(id)})
+        #doc_json = json_util.dumps(doc)
+        #return render_template('doc.html' , doc = doc_json )
+        return render_template('update.html' , doc = doc )
+        #return "<p>You requested a document with ID" + id +'<p>/n ' + json_util.dumps(doc)
+    else:
+        return 'Bad request'
+    
+    
+@mod_main.route('/update', methods=["GET",'POST'])
+def update_doc():
+        db = mongo.db.arkep
+        data = request.form.to_dict()
+        id = data['_id']
+        del data['_id']
+        #db.insert(data)
+        db.update(
+           {'_id':ObjectId(id)},
+          data,True
+        )
+        if (data):
+            result = "Te dhenat u ruajtun me sukses"
+            return redirect(url_for('main.lista'))
+
+        else:
+            result = "Ka ndodhur gabim ne ruajten e te dhenave"
+            return render_template('lista.html', result=result, json=json_util.dumps(data))
